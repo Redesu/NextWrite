56c3fcafc152ae8c3ca53ef87a5c92c4725dbe56
Willyan David <62126803+Redesu@users.noreply.github.com>
2025-07-30 22:27:41 -0300

    Fixed building the app
    App was not building because of TypeError

--- next-write/src/app/blog/[slug]/page.tsx
+++ next-write/src/app/blog/[slug]/page.tsx
@@ -4,9 +4,9 @@ import remarkHtml from "remark-html";
 import remarkGfm from "remark-gfm";
 import BlogPostClient from "./BlogPostClient";
 
-export default async function BlogPost({ params }: { params: { slug: string } }) {
-    const param = await params;
-    const post = await getPostBySlug(param.slug);
+export default async function BlogPost({ params }: { params: Promise<{ slug: string }> }) {
+    const { slug } = await params;
+    const post = await getPostBySlug(slug);
     const contentHtml = await remark().use(remarkGfm).use(remarkHtml).process(post.content);
 
     return (
--- next-write/src/app/edit-post/[slug]/page.tsx
+++ next-write/src/app/edit-post/[slug]/page.tsx
@@ -2,7 +2,7 @@
 import { getPostBySlug } from "@/lib/posts";
 import EditPostClient from "./EditPostClient";
 
-export default async function EditPostPage({ params }: { params: { slug: string } }) {
+export default async function EditPostPage({ params }: { params: Promise<{ slug: string }> }) {
     const { slug } = await params;
     const post = await getPostBySlug(slug);
 
