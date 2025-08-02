cb6f31864e416e8b2906b26f0c9f0750123ff90c
Willyan David <62126803+Redesu@users.noreply.github.com>
2025-08-01 23:02:21 -0300

    Update comments.routes.js

--- backend/src/routes/comments.routes.js
+++ backend/src/routes/comments.routes.js
@@ -1,9 +1,9 @@
 import { Router } from 'express';
-import { addComment, getComments } from '../controllers/index.js';
+import { newComment, getComments } from '../controllers/index.js';
 import { protect } from '../middleware/auth.middleware.js';
 
 const router = Router();
 
-router.post('/:postSlug', protect, addComment);
+router.post('/:postSlug', protect, newComment);
 router.get('/:postSlug', getComments);
 export default router;
