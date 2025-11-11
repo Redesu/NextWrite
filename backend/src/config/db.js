6bb674ca2bde37b6017c3b3ed3def71635110bf9
Willyan David <62126803+Redesu@users.noreply.github.com>
2025-11-11 19:49:51 -0300

    Handles db variable using .env

--- backend/src/config/db.js
+++ backend/src/config/db.js
@@ -1,23 +1,25 @@
-// import { Pool } from "pg";
-import { Pool, neonConfig } from "@neondatabase/serverless";
+import { neonConfig, Pool as NeonPool } from "@neondatabase/serverless";
+import { Pool as PgPool } from "pg";
 import ws from "ws";
-import "dotenv/config";
 
-neonConfig.webSocketConstructor = ws;
+const isProduction = process.env.NODE_ENV === "production";
+let pool;
 
-const pool = new Pool({
-  connectionString: process.env.DB_HOST,
-  ssl: true,
-});
-
-// If you are using a local database then use the following
-// const pool = new Pool({
-//     host: process.env.DB_HOST,
-//     port: process.env.DB_PORT,
-//     user: process.env.DB_USER,
-//     password: process.env.DB_PASSWORD,
-//     database: process.env.DB_NAME
-// });
+if (isProduction) {
+  neonConfig.webSocketConstructor = ws;
+  pool = new NeonPool({
+    connectionString: process.env.DB_HOST,
+    ssl: true,
+  });
+} else {
+  pool = new PgPool({
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+  });
+}
 
 export default {
   query: (text, params) => pool.query(text, params),
