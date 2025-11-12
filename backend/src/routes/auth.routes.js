81477e9bab32868146fe4bdda220eefba7642d5d
Willyan David <62126803+Redesu@users.noreply.github.com>
2025-11-11 21:54:47 -0300

    Fixes formatation

--- backend/src/routes/auth.routes.js
+++ backend/src/routes/auth.routes.js
@@ -1,16 +1,24 @@
-import express from 'express';
-import { Router } from 'express';
-import { register, login, logout, refreshToken, getMe, newComment, getComments } from '../controllers/index.js';
-import { protect } from '../middleware/auth.middleware.js';
+import express from "express";
+import { Router } from "express";
+import {
+  register,
+  login,
+  logout,
+  refreshToken,
+  getMe,
+  addComment,
+  getComments,
+} from "../controllers/index.js";
+import { protect } from "../middleware/auth.middleware.js";
 
 const router = Router();
 
-router.post('/register', register);
-router.post('/login', login);
-router.post('/logout', logout);
-router.post('/refresh-token', refreshToken);
-router.post('/:postSlug', protect, newComment);
-router.get('/me', protect, getMe);
-router.get('/:postSlug/comments', getComments);
+router.post("/register", register);
+router.post("/login", login);
+router.post("/logout", logout);
+router.post("/refresh-token", refreshToken);
+router.post("/:postSlug", protect, addComment);
+router.get("/me", protect, getMe);
+router.get("/:postSlug/comments", getComments);
 
 export default router;
--- backend/src/routes/posts.routes.js
+++ backend/src/routes/posts.routes.js
@@ -1,13 +1,19 @@
 import { Router } from "express";
-import { addPost, deletePost, getPosts, getPost, updatePost } from "../controllers/index.js";
+import {
+  addPost,
+  deletePost,
+  getPosts,
+  getPost,
+  updatePost,
+} from "../controllers/index.js";
 import { protect } from "../middleware/auth.middleware.js";
 
 const router = Router();
 
-router.post('/:postSlug', protect, addPost);
-router.delete('/:postSlug', protect, deletePost);
-router.get('/', getPosts);
-router.get('/:postSlug', getPost);
-router.put('/:postSlug', protect, updatePost);
+router.post("/:postSlug", protect, addPost);
+router.delete("/:postSlug", protect, deletePost);
+router.get("/", getPosts);
+router.get("/:postSlug", getPost);
+router.put("/:postSlug", protect, updatePost);
 
 export default router;
