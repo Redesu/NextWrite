3392637dc2bfbdff9c4bfce3cb840f1e1a1e2377
Willyan David <62126803+Redesu@users.noreply.github.com>
2025-11-11 23:06:47 -0300

    Update db.js

--- backend/src/config/db.js
+++ backend/src/config/db.js
@@ -14,7 +14,7 @@ if (isProduction) {
 } else {
   pool = new PgPool({
     host: process.env.DB_HOST,
-    port: process.env.DB_PORT,
+    port: Number(process.env.DB_PORT),
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME,
