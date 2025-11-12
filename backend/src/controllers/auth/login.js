4f603de11cea4292aa6a53a879ab80d5bc30ae6a
Willyan David <62126803+Redesu@users.noreply.github.com>
2025-11-11 23:54:30 -0300

    Update login.js

--- backend/src/controllers/auth/login.js
+++ backend/src/controllers/auth/login.js
@@ -31,14 +31,14 @@ export const login = async (req, res) => {
     //set the cookies
     res.cookie("accessToken", accessToken, {
       httpOnly: true,
-      secure: process.env.NODE_ENV === "production" ? true : "lax",
+      secure: process.env.NODE_ENV === "production",
       sameSite: "none",
       maxAge: 15 * 60 * 1000,
     });
 
     res.cookie("refreshToken", refreshToken, {
       httpOnly: true,
-      secure: process.env.NODE_ENV === "production" ? true : "lax",
+      secure: process.env.NODE_ENV === "production",
       sameSite: "none",
       maxAge: 7 * 24 * 60 * 60 * 1000,
     });
