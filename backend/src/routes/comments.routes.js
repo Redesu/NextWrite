58228ff5dd9d948e3407829e9704b2c8e75abe60
Willyan David <62126803+Redesu@users.noreply.github.com>
2025-11-11 21:54:42 -0300

    Adds comments routes

--- backend/src/routes/comments.routes.js
+++ backend/src/routes/comments.routes.js
@@ -1,9 +1,16 @@
-import { Router } from 'express';
-import { newComment, getComments } from '../controllers/index.js';
-import { protect } from '../middleware/auth.middleware.js';
+import { Router } from "express";
+import {
+  addComment,
+  getComments,
+  updateComment,
+  deleteComment,
+} from "../controllers/index.js";
+import { protect } from "../middleware/auth.middleware.js";
 
 const router = Router();
 
-router.post('/:postSlug', protect, newComment);
-router.get('/:postSlug', getComments);
+router.post("/:postSlug", protect, addComment);
+router.get("/:postSlug", getComments);
+router.delete("/delete/:commentId", protect, deleteComment);
+router.put("/update/:commentId", protect, updateComment);
 export default router;
